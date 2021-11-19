646887d2ec38a53f4b591130c6a7dbf665a2c958
Алексей <tsyplov80@mail.ru>
2021-11-19 18:34:56 +0300

    create_base_object

---
+++ src/station/immovable/RailwayTrack.java
@@ -0,0 +1,12 @@
+package station.immovable;
+
+public class RailwayTrack extends RailwayStationObject {
+
+    private RailwayStationObjectType type;
+    private int id;
+
+    public RailwayTrack() {
+        super(RailwayStationObjectType.RAILWAY_TRACK);
+    }
+
+}
