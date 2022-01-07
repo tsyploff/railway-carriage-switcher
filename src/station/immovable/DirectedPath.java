f451e920472d257f728b92c77bf26f2d375cd542
Алексей <tsyplov80@mail.ru>
2022-01-07 09:55:37 +0300

    first_xlsx_page

---
+++ src/station/immovable/DirectedPath.java
@@ -0,0 +1,66 @@
+package station.immovable;
+
+import java.util.ArrayList;
+
+/**
+ * DirectedPath это специальный класс, экземпляры которого представляют пары рёбер в графе
+ * железнодорожных путей на станции. А имеено, если с пути A можно переехать на путь B, то
+ * в графе будет содержаться ориентированный путь A, B.
+ *
+ * DirectedPath так же может быть использован для хранения маршрутов поездов, т. к. может
+ * содержать последовательность путей железнодорожного графа.
+ *
+ * Поскольку запуск программы и инициализация объектов происходит через Wolfram, выгоднее
+ * импользовать идентификаторы (т. е. примитивные типы) в аргументах.
+ */
+public class DirectedPath {
+    String from;
+    String to;
+
+    ArrayList<String> intermediateTracks;
+
+    public DirectedPath(String from, String to) {
+        this.from = from;
+        this.to = to;
+        this.intermediateTracks = new ArrayList<>();
+    }
+
+    /**
+     *  Добавляет промежуточные вершины в путь.
+     */
+    public void addIntermediateTrack(String trackName) {
+        this.intermediateTracks.add(trackName);
+    }
+
+    /**
+     * Возвращает список идентификаторов (!) железнодорожных путей, начиная с from и
+     * заканчивая to.
+     */
+    public ArrayList<String> getTrackNames() {
+        ArrayList<String> path = new ArrayList<>();
+        path.add(this.from);
+        path.addAll(this.intermediateTracks);
+        path.add(to);
+        return path;
+    }
+
+    /**
+     * Возвращает список железнодорожных путей (!), т. е. экземпляров класса RailwayTrack,
+     * у которых можно посмотреть длину, вместимость и т. д.
+     */
+    public ArrayList<RailwayTrack> getTracks() {
+        ArrayList<RailwayTrack> path = new ArrayList<>();
+        for (String trackName: this.getTrackNames()) {
+            path.add(RailwayTrack.get(trackName));
+        }
+        return path;
+    }
+
+    /**
+     * Путь называется простым, если не содержит промежуточных ж/д путей.
+     */
+    public boolean isSimple() {
+        return this.intermediateTracks.isEmpty();
+    }
+
+}
--- src/station/immovable/RailwayTrack.java
+++ src/station/immovable/RailwayTrack.java
@@ -1,12 +1,38 @@
 package station.immovable;
 
-public class RailwayTrack extends RailwayStationObject {
+import java.util.HashMap;
 
-    private RailwayStationObjectType type;
-    private int id;
+public class RailwayTrack {
 
-    public RailwayTrack() {
-        super(RailwayStationObjectType.RAILWAY_TRACK);
+    String trackName;
+    String switchStart;
+    String switchEnd;
+    int lengthMeters;
+    int wagonesCapacity;
+
+    final private static HashMap<String, RailwayTrack> trackNames = new HashMap<>(); // список всех путей
+
+    public RailwayTrack(String trackName, String switchStartName, String switchEndName, int lengthMeters) {
+        new RailwayTrack(trackName, switchStartName, switchEndName, lengthMeters, 0);
+    }
+
+    public RailwayTrack(
+            String trackName,
+            String switchStartName,
+            String switchEndName,
+            int lengthMeters,
+            int wagonesCapacity
+    ) {
+        this.trackName = trackName;
+        this.switchStart = switchStartName;
+        this.switchEnd = switchEndName;
+        this.lengthMeters = lengthMeters;
+        this.wagonesCapacity = wagonesCapacity;
+        trackNames.put(trackName, this);
+    }
+
+    public static RailwayTrack get(String trackName) {
+        return trackNames.get(trackName);
     }
 
 }
